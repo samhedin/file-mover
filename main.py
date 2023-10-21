6d117778f6bca1c345412504d8a3f02ff749ca3d
Sam Hedin <sam@instabridge.com>
2023-10-21 20:03:37 +0200

    a

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import shutil
 import pathlib
 
 def move(mappings):
-    m = {}
-    print("moving: ")
+    extension_to_folder = {}
     for extension, folder in zip(mappings[0::2], mappings[1::2]):
-        print(f"*{extension}->{folder}")
-        m[extension] = folder
+        extension_to_folder[extension] = folder
 
     for f in os.listdir():
         _, extension = os.path.splitext(f)
-        if extension in m:
-            shutil.copy(f, m[extension])
+        if extension in extension_to_folder:
+            folder = extension_to_folder[extension]
+            print(f"{f} -> {folder}")
+            shutil.copy(f, folder)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='file-mover',
-        description='Ge par av filändelse, mapp Exempel: .jpg, jpegs, .png, pngs för att skicka alla .jpg till jpegs-mappen och .png till pngs-mappen',)
+        description='Ge par av filändelse, mapp Exempel: python main.py .jpg jpegs .png pngs för att skicka alla .jpg till jpegs-mappen och .png till pngs-mappen',)
     parser.add_argument("mapping", nargs="+")
     args = parser.parse_args()
     mapping = args.mapping
