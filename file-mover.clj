41dd5a831cd234cfa8fac239bfb33d7a0967cc96
Sam Hedin <sam@instabridge.com>
2023-10-25 17:43:02 +0200

    More description

--- file-mover.clj
+++ file-mover.clj
@@ -3,18 +3,19 @@
          '[babashka.fs :as fs]
          '[clojure.string :as string])
 
-(def spec 
+(def spec
   {:mappings
    {:coerce []
     :alias :m
-    :desc "List of file extensions and folders. Each file with a specified extension will be moved to the related folder. List must be of even length. Example: --mappings jpg jpgfolder png pngfolder"
+    :desc "List of file extensions and folders. \nEach file with a specified extension will be moved to the related folder. List must be of even length.\n Example: --mappings jpg jpgfolder png pngfolder\n"
     :validate {:pred #(even? (count %))
                :ex-msg (fn [{:keys [value]}]
                          (str "Each file extension needs to have a folder to map to. You provided " value " which should be of even length, but is not."))}}
 
    :fromdirectory
-   {:desc "Directory with all files that should be moved, defaults to current directory"
+   {:desc "Directory containing all files that should be moved\n"
     :alias :d
+    :default-desc (str "Default is current directory: " (fs/cwd) "\n")
     :default (str (fs/cwd))}
 
    :help
@@ -23,12 +24,14 @@
     :default false}})
 
 (def example-input ["--mappings" "aiff" "example/maiff" "jpeg" "example/jpe"
-                            "--fromdirectory" "example" "-h"])
-(def input (cli/parse-opts *command-line-args*
+                    "--fromdirectory" "example"])
+
+(def input (cli/parse-opts (or *command-line-args* example-input)
                            {:spec spec}))
 
 (when (:help input)
-  (println (cli/format-opts {:spec spec :order [:mappings :fromdirectory]}))
+  (println "This program helps you organize your files by moving them based on file extension and size.\n")
+  (println (cli/format-opts {:spec spec}))
   (System/exit 0))
 
 (def ext->folder "Map from file extension to location."
