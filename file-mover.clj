1624b13587f093003e0e144211a7a180d4462f93
Sam Hedin <sam@instabridge.com>
2023-10-25 17:13:02 +0200

    Spec input

--- file-mover.clj
+++ file-mover.clj
@@ -3,22 +3,33 @@
          '[babashka.fs :as fs]
          '[clojure.string :as string])
 
-(def spec {:coerce {:mappings []}
-           :validate {:mappings
-                      {:pred #(even? (count %))
-                       :ex-msg (fn [{:keys [value]}]
-                                 (str "Each file extension needs to have a folder to map to. You provided " value " which should be of even length, but is not."))}}
-           :exec-args {:fromdirectory (fs/cwd)}})
+(def spec 
+  {:mappings
+   {:coerce []
+    :alias :m
+    :desc "List of file extensions and folders. Each file with a specified extension will be moved to the related folder. List must be of even length. Example: --mappings jpg jpgfolder png pngfolder"
+    :validate {:pred #(even? (count %))
+               :ex-msg (fn [{:keys [value]}]
+                         (str "Each file extension needs to have a folder to map to. You provided " value " which should be of even length, but is not."))}}
 
-(prn (cli/parse-args *command-line-args*
-                     spec))
+   :fromdirectory
+   {:desc "Directory with all files that should be moved, defaults to current directory"
+    :alias :d
+    :default (str (fs/cwd))}
 
-(cli/parse-args ["--mappings" "aiff" "example/maiff" "jpeg" "example/jpeg"
-                 "--fromdirectory" "example"]
-                spec)
+   :help
+   {:desc "Get help"
+    :alias :h
+    :default false}})
 
-(def input {:args ["aiff" "example/maiff" "jpeg" "example/jpeg"]
-            :opts {:fromdirectory "example"}})
+(def example-input ["--mappings" "aiff" "example/maiff" "jpeg" "example/jpe"
+                            "--fromdirectory" "example" "-h"])
+(def input (cli/parse-opts *command-line-args*
+                           {:spec spec}))
+
+(when (:help input)
+  (println (cli/format-opts {:spec spec :order [:mappings :fromdirectory]}))
+  (System/exit 0))
 
 (def ext->folder "Map from file extension to location."
   (apply hash-map (:args input)))
